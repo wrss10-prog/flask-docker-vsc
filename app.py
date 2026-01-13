b04863c2e3f44d437909b64e2a38194a902b5dd1
roger <wrss10@gmail.com>
2026-01-13 14:02:50 -0300

    teste

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Flask + Docker funcionou!"
+    return "Hello Flask + Docker funcionou! kkk"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
